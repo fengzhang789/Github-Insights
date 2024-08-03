b3cc3c476a774ac331e0445de709a923692717a4
allanli608 <allanli608@gmail.com>
2024-08-03 15:16:11 -0400

    finished making scrollbar (for now)

--- frontend/app/dashboard/page.tsx
+++ frontend/app/dashboard/page.tsx
@@ -1,32 +1,28 @@
 "use client";
 
 import React from "react";
-import CommitHistoryView from './__components/CommitHistoryView';
+import CommitHistoryView from "./__components/CommitHistoryView";
 import TopBar from "../__components/Topbar";
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../__store/store";
 
 export default function Dashboard() {
-
   const currentView = useSelector((state: RootState) => state.view.currentView);
 
   return (
     <div>
-      <TopBar/>
-      {currentView == null && 
+      <TopBar />
+      {currentView == null && (
         <div className="h-[80svh] grid place-items-center">
-        <h1 className="text-5xl font-semibold text-center">
-          Select a Repository to Start.
-        </h1>
+          <h1 className="text-5xl font-semibold text-center">
+            Select a Repository to Start.
+          </h1>
         </div>
-      }
-      {currentView == 'project' &&
-      'project'}
-      {currentView == 'feature' &&
-      'feature'}
-      {currentView == 'user' &&
-      'user'}
-      <CommitHistoryView/>
+      )}
+      {currentView == "project" && "project"}
+      {currentView == "feature" && "feature"}
+      {currentView == "user" && "user"}
+      <CommitHistoryView />
     </div>
   );
 }
