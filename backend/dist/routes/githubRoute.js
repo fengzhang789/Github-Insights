59313bc5e473d5fcdb538c446fa3b36f381ae77d
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-08-03 12:05:54 -0400

    added individual commit data, plus diff data

--- backend/dist/routes/githubRoute.js
+++ backend/dist/routes/githubRoute.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { handleGetAppInformationRequest, handleGetAppInstallations, handleGetAppRepositoryInformation, handleGetRepositoryCommits, handleGetUserRepositories, handleLoginGithub } from "../controllers/githubController.js";
+import { handleGetAppInformationRequest, handleGetAppInstallations, handleGetAppRepositoryInformation, handleGetRepositoryCommit, handleGetRepositoryCommitDiff, handleGetRepositoryCommits, handleGetUserRepositories, handleLoginGithub } from "../controllers/githubController.js";
 export const router = Router();
 router.get('/app', handleGetAppInformationRequest);
 router.get('/app/repositories/all', handleGetAppRepositoryInformation);
 router.get('/installations/all', handleGetAppInstallations);
 router.post('/user/repositories', handleGetUserRepositories);
 router.post('/login', handleLoginGithub);
-router.get('/repository/commits', handleGetRepositoryCommits);
+router.post('/repository/commits', handleGetRepositoryCommits); // gets the first commit of a repository
+router.post('/repository/commit/:ref', handleGetRepositoryCommit); // gets a specific commit of a repository
+router.post('/repository/commit/:ref/diff', handleGetRepositoryCommitDiff); // gets all commits of a repository
--- backend/src/routes/githubRoute.ts
+++ backend/src/routes/githubRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { handleGetAppInformationRequest, handleGetAppInstallations, handleGetAppRepositoryInformation, handleGetRepositoryCommits, handleGetUserRepositories, handleLoginGithub } from "../controllers/githubController.js";
+import { handleGetAppInformationRequest, handleGetAppInstallations, handleGetAppRepositoryInformation, handleGetRepositoryCommit, handleGetRepositoryCommitDiff, handleGetRepositoryCommits, handleGetUserRepositories, handleLoginGithub } from "../controllers/githubController.js";
 
 export const router = Router();
 
@@ -8,4 +8,6 @@ router.get('/app/repositories/all', handleGetAppRepositoryInformation);
 router.get('/installations/all', handleGetAppInstallations);
 router.post('/user/repositories', handleGetUserRepositories);
 router.post('/login', handleLoginGithub);
-router.post('/repository/commits', handleGetRepositoryCommits);
+router.post('/repository/commits', handleGetRepositoryCommits);     // gets the first commit of a repository
+router.post('/repository/commit/:ref', handleGetRepositoryCommit);       // gets a specific commit of a repository
+router.post('/repository/commit/:ref/diff', handleGetRepositoryCommitDiff);     // gets all commits of a repository
