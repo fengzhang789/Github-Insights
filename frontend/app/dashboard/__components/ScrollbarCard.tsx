7aad8b25dab0de5ff195fae3c3c865cb9eeeebdd
YunFeng Zhang <73557467+fengzhang789@users.noreply.github.com>
2024-08-04 07:19:01 -0400

    removed env

--- frontend/app/dashboard/__components/ScrollbarCard.tsx
+++ frontend/app/dashboard/__components/ScrollbarCard.tsx
@@ -21,7 +21,8 @@ export default function ScrollbarCard({
         <CardHeader>
 
           <CardTitle className="text-lg font-semibold truncate-multiline">
-            Commit Message: {commit.message}
+            {commit.message}
+            <Image srhrefc="https://www.svgrepo.com/show/327279/git-branch.svg" alt="git branch" />
           </CardTitle>
 
           <div className="flex items-center mb-2">
