149f45a1fea5161dd8c7ab1478b47246a2b7932d
allanli608 <allanli608@gmail.com>
2024-08-03 22:20:27 -0400

    added push to login function

--- frontend/app/clientLayout.tsx
+++ frontend/app/clientLayout.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
-import ReduxProvider from './__store/ReduxProvider';
+"use client";
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useCookies } from "react-cookie";
+import ReduxProvider from "./__store/ReduxProvider";
 
 type Props = {
   children: React.ReactNode;
-}
+};
 
-const clientLayout = (props: Props) => {
-  return (
-    <ReduxProvider>
-      {props.children}
-    </ReduxProvider>
-  )
-}
+const validPaths = ["/", "/login", "/login/github"];
 
-export default clientLayout
+const ClientLayout = (props: Props) => {
+  const router = useRouter();
+  const [cookies] = useCookies(["accessJwt"]);
+
+  useEffect(() => {
+    const currentPath = window.location.pathname;
+    const token = cookies.accessJwt;
+
+    if (!token && !validPaths.includes(currentPath)) {
+      router.push("/login");
+    }
+  }, [cookies, router]);
+
+  return <ReduxProvider>{props.children}</ReduxProvider>;
+};
+
+export default ClientLayout;
